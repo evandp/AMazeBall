151a57a1ba52700511255f3def1021c1c1e7c6e6
Evan Patrick <evandp1999@gmail.com>
2019-05-14 13:51:57 -0400

    getting LED matrix to work

---
+++ matrix_controller.h
@@ -0,0 +1,34 @@
+#include <fsl_device_registers.h>
+#include <fsl_gpio.h>
+
+#define clear_clk() (GPIO_PinWrite(clk_p, clk_s, 0))
+#define set_clk() (GPIO_PinWrite(clk_p, clk_s, 1))
+#define clear_lat() (GPIO_PinWrite(lat_p, lat_s, 0))
+#define set_lat() (GPIO_PinWrite(lat_p, lat_s, 1))
+#define clear_oe() (GPIO_PinWrite(oe_p, oe_s, 0))
+#define set_oe() (GPIO_PinWrite(oe_p, oe_s, 1))
+
+class MatrixController {
+	
+	public:	
+		static void update_pixel(int x, int y, bool r, bool g, bool b);
+		static void write_matrix();
+		static void init_matrix();
+		static void init_timer();
+		static void init_gpio();
+		typedef struct {
+			bool r;
+			bool g;
+			bool b;
+		} pixel_t;
+
+	private:
+		static pixel_t matrix[32][32];
+		static void set_rgb();
+		static void clear_row();
+		static void set_row(int row);
+		static int rgb_to_bits(int r, int g, int b);
+		static void clear_rgb();
+		static void set_rgb_top(int c);
+		static void set_rgb_bottom(int c);
+};
