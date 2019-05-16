c50c7d8b6854c61fa6421e321dfb72fa971d3f6e
Natalie Neamtu <nan55@cornell.edu>
2019-05-16 19:35:50 -0400

    comments

--- IMU.cpp
+++ IMU.cpp
@@ -1,9 +1,18 @@
 #include "IMU.h"
 
+/* This is the sensor driver for the Adafruit BNO055 IMU. */
 Adafruit_BNO055 sensor = Adafruit_BNO055(BNO055_ID, BNO055_ADDRESS_A);
+
+/* The data from the IMU gets read into this struct. */
 sensors_event_t * event = (sensors_event_t *) malloc(sizeof(sensors_event_t));
+
+/* This struct stores the running averages for pitch and roll, where the average is "reset" once the data is read via get_pitch() or get_roll(). */
 volatile IMU::orientation_vec data = {0, 0};
+
+/* Keeps track of number of times the IMU has been polled for pitch data since the last get_pitch() call. */
 int num_reads_p = 0;
+
+/* Keeps track of number of times the IMU has been polled for roll data since the last get_roll() call. */
 int num_reads_r = 0;
 
 IMU::IMU() {
@@ -14,11 +23,19 @@ IMU::~IMU(){
 	free(event);
 }
 
+
+/*
+	Starts up the sensor driver and starts the PIT1 interrupts to poll the IMU.
+	This function causes a small delay due to the set up of the sensor.
+*/
 void IMU::begin() {
 	sensor.begin();
 	IMU::begin_timer();
 }
 
+/*
+	Sets up PIT1 to cause an interrupt 30 times per second.
+*/
 void IMU::begin_timer(){
 	int rate = 30;
 	
@@ -32,6 +49,11 @@ void IMU::begin_timer(){
 	PIT->CHANNEL[1].TCTRL |= PIT_TCTRL_TEN(1);
 }
 
+/*
+	Returns the running average of the IMU pitch data since the last
+	call to this function. Resets the running average so that num_reads_p 
+	is now 0.
+*/
 double IMU::get_pitch(){
 	__disable_irq();
 	
@@ -42,6 +64,11 @@ double IMU::get_pitch(){
 	return data.pitch;
 }
 
+/*
+	Returns the running average of the IMU roll data since the last
+	call to this function. Resets the running average so that num_reads_r
+	is now 0.
+*/
 double IMU::get_roll(){
 	__disable_irq();
 	
@@ -52,6 +79,11 @@ double IMU::get_roll(){
 	return data.roll;
 }
 
+/*
+	Polls the IMU sensor for pitch and roll data and incorporates the 
+	results into the running averages stored by the data struct.
+	This function is used by the PIT1 interrupt.
+*/
 void IMU::read_data(){
 	sensor.Adafruit_BNO055::getEvent(event);
 	if (num_reads_r > 0) {
@@ -68,6 +100,11 @@ void IMU::read_data(){
 	num_reads_p++;
 }
 
+/*
+	Interrupt handler for PIT1. Causes the IMU driver to poll the sensor
+	and add the new data to the running averages for pitch and roll stored
+	by the IMU driver.
+*/
 extern "C" void PIT1_IRQHandler(void)
 {
 	PIT->CHANNEL[1].TCTRL = PIT_TCTRL_TEN(0);
--- IMU.h
+++ IMU.h
@@ -5,21 +5,43 @@
 #include <fsl_device_registers.h>
 #include "Adafruit_BNO055.h"
 
+
+/* 
+	This class is the interface between the Adafruit BNO055 driver and the game driver.
+	Its main responsibility is to periodically poll the IMU for data, and keep a running
+	average of this data until it is read by the game driver. 
+*/
 class IMU {
 
 public:
+	
+	/* Stores data from IMU */
 	typedef struct {
 		double pitch;
 		double roll;
 	} orientation_vec;
+	
+	/* Constructor for IMU (useless because all members are static) */
 	IMU();
+	
+	/* Destructor for IMU (useless because all members are static) */
 	~IMU();
+	
+	/* Returns the filtered (i.e. averaged) pitch data.	*/
 	static double get_pitch();
+	
+	/* Returns the filtered (i.e. averaged) roll data.	*/
 	static double get_roll();
+	
+	/* Initializes IMU driver and begins reading data via PIT interrupt. */
 	static void begin();
+	
+	/* Polls data from Adafruit BNO055 device for roll and pitch. */
 	static void read_data();
 	
 private:
+	
+	/* Sets up timer interrupt on PIT1. */
 	static void begin_timer();
 
 };
